614ca5089f8dfd0fe9f136200876610808abf173
조민규 <inch1209@naver.com>
2022-01-04 16:40:46 +0900

    configuration

---
+++ src/test/java/first/firstspring/FirstSpringApplicationTests.java
@@ -0,0 +1,14 @@
+package first.firstspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringApplicationTests {
+
+	public static void main(String[] args){
+		SpringApplication.run(FirstSpringApplication.class,args);
+	}
+
+}
